313c6c2252d2f8f13d01cf6fedd739f23dea4cba
slikc <82408235+slikc@users.noreply.github.com>
2022-12-30 23:01:27 +0000

    Update ravelry.py

--- ravelry.py
+++ ravelry.py
@@ -4,8 +4,16 @@ import tempmail
 
 count = 0
 
+
 def main():
-    email, password = tempmail.create_account("karenkey.com")
+    timerr = 0
+    st = time.time()
+    mail = tempmail.create_email()
+    if mail == "RateLimit":
+        print(f"{Fore.RED}[ERROR]{Fore.RESET} RateLimited")
+        time.sleep(30)
+        main()
+    email = mail[0]
     session = requests.Session()
     username = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
     ravelpassword = ("").join(random.choices(string.ascii_letters + string.digits, k = 8))
@@ -17,7 +25,6 @@ def main():
         "guest": "Y",
         "email": email
     }
-    time.sleep(0.3)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
         "accept-language": "en-GB,en;q=0.9",
@@ -28,11 +35,21 @@ def main():
         "sec-ua-mobile": "?0"
     }
     x = session.post("https://www.ravelry.com/invitations/ask", headers=headers, data=payload)
-    print(x.status_code)
-    #check the emails
-    authheaders = tempmail.get_jwt(email, password)
-    time.sleep(0.2)
-    code = tempmail.check_mail(authheaders)
+    time.sleep(1)
+    token = mail[1]
+    code = tempmail.get_messages(token)
+    if code == False:
+        while True:
+            code = tempmail.get_messages(token)
+            if code != False:
+                break
+            else:
+                time.sleep(3)
+                timerr += 1
+                print("No code found")
+                if timerr == 5:
+                    print("No code found")
+                    continue
     payload = {
         "authenticity_token": authenticity_token,
         "_method": "put",
@@ -43,21 +60,21 @@ def main():
         "user[password_confirmation]": ravelpassword,
     }
     verify = session.post(f"https://www.ravelry.com/people/JessicaMF/invite/{email}", headers=headers, data=payload)
-    time.sleep(0.3)
     checkuser = session.get(f"https://www.ravelry.com/people/{username}")
     if checkuser.status_code == 404:
         print(f"{Fore.RED}[-] Error creating account{Fore.RESET}")
     else:
-        print(f"{Fore.GREEN}[+]{Fore.RESET} Account Created - " + email + ":" + ravelpassword + " - " + username)
+        et = time.time()
+        print(f"{Fore.GREEN}[+]{Fore.RESET} Account Created - " + email + ":" + ravelpassword + " - " + username + " - Took " + str(et - st) + " seconds\n")
         with open("ravelry.txt", "a") as f:
-            f.write(email + ":" + ravelpassword + ":" + username + "\n")
+            f.write(email + ":" + username + ":" + ravelpassword + "\n")
 
 
 
-for i in range(1000):
+for i in range(10000):
     count+=1
-    if count == 10:
-        print("Waiting 10 seconds")
-        time.sleep(30)
+    if count == 15:
+        print(f"{Fore.BLUE}[INFO]{Fore.RESET} WAITING 15 SECONDS FOR RATE LIMIT")
+        time.sleep(20)
         count = 0
     main()
